2607132ec60f692ca110035b92d911e47d3621cb
maxell <maxell@student.21-school.ru>
2020-04-03 18:34:53 +0300

    initial commit updated

--- header.h
+++ header.h
@@ -4,16 +4,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int is_valid(int ac, char **ag);
 int **create_map(char **ag);
 
 int sudoku_solver(int **map);
+
 int is_null(int **map, int *row, int *col);
-
-int roww(int **map, int row, int num);
-int column(int **map, int col, int num);
+int row_column(int **map, int row, int col, int num);
 int square(int **map, int row, int col, int num);
-
 int is_safe(int **map, int row, int col, int num);
 
 void sudoku_print(int **map);
